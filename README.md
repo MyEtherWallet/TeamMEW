88f70246a725bc5978f587b4ef7aee872f71c25a
gamalielhere <gamalielgad1234@gmail.com>
2018-06-04 11:34:10 -0700

    Updates

---
+++ README.md
@@ -0,0 +1,24 @@
+# TeamMEW
+Team MEW page
+### Installation
+
+Dillinger requires [Node.js](https://nodejs.org/) v9+ to run.
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+$ cd TeamMEW
+$ npm install
+$ npm run dev
+```
+
+For production environments...
+
+```sh
+$ npm install
+$ npm run build
+```
+Maintained By:\
+[@kvhnuke](https://github.com/kvhnuke)\
+[@SteveMieskoski](https://github.com/SteveMieskoski)\
+[@gamalielhere](https://github.com/gamalielhere)
---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="team">
+    
+    <p class="title">#TeamMEW</p>
+    <p class="sub-title">MyEtherWallet</p>
+
+    <div class="team-images">
+      <li v-for="person in mainList">
+        <img :src="'dist/'+person.image">
+        <div class="info">
+          <p class="name">{{person.name}}</p>
+          <p class="position">{{person.position}}</p>
+        </div>
+      </li>
+    </div>
+
+  </div>
+</template>
+<script>
+  import mainList from "@/lists/main-team.json"
+  console.log(mainList)
+  export default {
+  name: 'app',
+  data () {
+    return {
+      mainList: mainList
+    }
+  },
+  computed: {
+    imagePath: function (imgName) {
+      return require('@/img/' + imgName)
+    }
+  }
+}
+</script>
+
+<style lang="less">
+  @import "./styles/styles.less";
+</style>
